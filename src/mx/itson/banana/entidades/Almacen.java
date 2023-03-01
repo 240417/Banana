8a4976a4c781abc92bc2fc9d9b324f79ed2b6509
medin <medin@10.232.4.33>
2023-03-01 12:15:40 -0700

    La presentción lista Se creo la clase tipo de existencia Se creo Almacen

---
+++ src/mx/itson/banana/entidades/Almacen.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.itson.banana.entidades;
+
+/**
+ *
+ * @author medin
+ */
+public class Almacen {
+    private String nombre;
+    private Producto producto;
+}
