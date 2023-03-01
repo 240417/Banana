bdb6140370a6c3d12ce5264e9be6d24790c3253b
medin <medin@10.232.4.33>
2023-03-01 12:08:58 -0700

    La presentción lista

---
+++ src/mx/itson/banana/entidades/Producto.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.itson.banana.entidades;
+
+/**
+ *
+ * @author medin
+ */
+public class Producto {
+    private String nombre ;
+    private Double precio;
+    private int id;
+    private Existencia existencia;
+}
---
+++ src/mx/itson/banana/exception/BananaException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.itson.banana.exception;
+
+/**
+ *
+ * @author medin
+ */
+public class BananaException extends Exception{
+     public BananaException(String errorMessage){
+        super(errorMessage);
+    }
+}
